5548feaa07cb5eb9ad781a84dd42ab010ebb95ec
fdolzanes1 <fdolzanes1@gmail.com>
2020-02-22 18:13:19 -0400

    Display single and All Documents

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,6 +1,9 @@
 class DocsController < ApplicationController
 
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
+
   def index
+    @docs = Doc.all.order("created_at DESC")
   end
 
   def show 
@@ -24,14 +27,22 @@ class DocsController < ApplicationController
   end 
 
   def update
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else 
+      render "edit"
+    end
   end 
 
   def destroy
+    @doc.destroy
+    redirect_to docs_path
   end
 
   private 
     
     def find_doc
+      @doc = Doc.find(params[:id])
     end
 
     def doc_params
