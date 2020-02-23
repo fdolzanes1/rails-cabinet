b9ddb66201b6b3a617a4ea4bdf10e648f0fe2822
fdolzanes1 <fdolzanes1@gmail.com>
2020-02-23 18:15:41 -0400

    Association between User and Docs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  root to: "welcome#index"
   get 'welcome/index'
   resources :docs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
