23e23816b40ba4b1d3b4d8711403f3efc1552ee1
fdolzanes1 <fdolzanes1@gmail.com>
2020-03-02 18:46:56 -0400

    Fix bugs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+'Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -91,4 +91,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.serve_static_assets = false
 end
